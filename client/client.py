58a698955e1b7760c6093566e9f0c40f311ffbfa
Umidjon <hackerumif7@gmail.com>
2022-11-14 10:51:52 +0500

    Client app should be changed

--- client/client.py
+++ client/client.py
@@ -1,42 +1,42 @@
-""" Client module that has several modes for usage.
+# """ Client module that has several modes for usage.
 
-    For now only CMD mode can be used. On this mode the user can
-    execute OS commands right on this client app.
-"""
+#     For now only CMD mode can be used. On this mode the user can
+#     execute OS commands right on this client app.
+# """
 
-from cmd.cmd_parser import cmdapp
+# from cmd.cmd_parser import cmdapp
 
 
-def main():
-    """ Main function with high level logic of the client app.
-        Always waits for a mode, which is an app, to be selected.
-    """
+# def main():
+#     """ Main function with high level logic of the client app.
+#         Always waits for a mode, which is an app, to be selected.
+#     """
 
-    client_modes = ["CMD", "Connect to server", "Exit"]
+#     client_modes = ["CMD", "Connect to server", "Exit"]
 
-    while True:
-        print("Modes:")
-        for i, option in zip(range(len(client_modes)), client_modes):
-            print(f"    {i}: {option}")
+#     while True:
+#         print("Modes:")
+#         for i, option in zip(range(len(client_modes)), client_modes):
+#             print(f"    {i}: {option}")
 
-        s = f"Choose one of the modes (0-{len(client_modes)-1}): "
-        chosen_mode = input(s)
+#         s = f"Choose one of the modes (0-{len(client_modes)-1}): "
+#         chosen_mode = input(s)
 
-        match chosen_mode:
-            case "0":
-                cmdapp()
+#         match chosen_mode:
+#             case "0":
+#                 cmdapp()
             
-            case "1":
-                print("Not implemented yet")
+#             case "1":
+#                 print("Not implemented yet")
             
-            case "2":
-                print("Finishing the client program...")
-                break
+#             case "2":
+#                 print("Finishing the client program...")
+#                 break
 
-            case _:
-                print("Unknown command")
+#             case _:
+#                 print("Unknown command")
 
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
